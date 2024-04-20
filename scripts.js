f69b54145bf7509f41e1b6e96e3ff67d1997373b
futuretrendbike <150936106+futuretrendbike@users.noreply.github.com>
2024-04-21 01:31:39 +0800

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,20 +1,18 @@
-document.addEventListener('DOMContentLoaded', function () {
-    fetch('https://docs.google.com/spreadsheets/d/16MaWeEJ7hcAIbHKGGYlInP3inWNRvzWYvbVw0CH9Iwk/pub?output=csv')
-        .then(response => response.json())
-        .then(data => {
-            const tableBody = document.querySelector('#data-table tbody');
-            data.forEach(row => {
-                const newRow = document.createElement('tr');
-                newRow.innerHTML = `
-                    <td>${row[0]}</td>
-                    <td>${row[1]}</td>
-                    <td>${row[2]}</td>
-                    <td>${row[3]}</td>
-                    <td>${row[4]}</td>
-                    <td>${row[5]}</td>
-                `;
-                tableBody.appendChild(newRow);
-            });
-        })
-        .catch(error => console.error('Error fetching data:', error));
-});
+fetch('https://docs.google.com/spreadsheets/d/16MaWeEJ7hcAIbHKGGYlInP3inWNRvzWYvbVw0CH9Iwk/pub?output=csv')
+    .then(response => response.json())
+    .then(data => {
+        const tableBody = document.querySelector('#data-table tbody');
+        data.forEach(row => {
+            const newRow = document.createElement('tr');
+            newRow.innerHTML = `
+                <td>${row[0]}</td>
+                <td>${row[1]}</td>
+                <td>${row[2]}</td>
+                <td>${row[3]}</td>
+                <td>${row[4]}</td>
+                <td>${row[5]}</td>
+            `;
+            tableBody.appendChild(newRow);
+        });
+    })
+    .catch(error => console.error('Error fetching data:', error));
